d07452abd0ab5555ca18d228c4f75b20b5c956d2
shritish20 <shritish@amityonline.com>
2025-06-14 08:47:49 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Start Uvicorn, listening on all interfaces, port 8000
-# 'main:app' assumes your FastAPI app instance is named 'app' in 'main.py'
-# If your app is in a different file (e.g., `your_api_file.py`), change it to `your_api_file:app`
-exec uvicorn main:app --host 0.0.0.0 --port 8000
